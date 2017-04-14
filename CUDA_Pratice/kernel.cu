ead427761e3c12f38ff6ad626eb39ad8a5c25840
Kiwi <felicidad20120706@gmail.com>
2017-04-14 15:24:22 +0800

    CUDA_Homework Backup

---
+++ CUDA_Pratice/kernel.cu
@@ -0,0 +1,193 @@
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+#include <cstdlib>
+#include <ctime>
+#include <ostream>
+#include <iostream>
+#define DATA_SIZE 1048576
+#define THREAD_NUM 256
+
+int data[DATA_SIZE];
+bool InitCUDA();
+void GenerateNumbers(int *number, int size);
+void ArrayCompute();
+void ArrayCompute_multiple_threads();
+__global__ static void sumOfSquares(int *num, int* result);
+__global__ static void sumOfSquares_multiple_threads(int *num, int* result);
+
+int main()
+{
+	if (InitCUDA())
+	{
+		ArrayCompute();
+		ArrayCompute_multiple_threads();
+	}
+
+
+	return 0;
+}
+
+bool InitCUDA()
+{
+	int count;
+
+	cudaGetDeviceCount(&count);
+	if (count == 0)
+	{
+		fprintf(stderr, "No Device!!");
+		return false;
+	}
+
+	int i;
+	for (i = 0; i < count; i++)
+	{
+		cudaDeviceProp prop;
+		if (cudaGetDeviceProperties(&prop, i) == cudaSuccess)
+		{
+			if (prop.major >= 1)
+			{
+				printf("第%d張顯卡名稱 -> %s\n", count, prop.name);
+				printf("--CUDA版本 -> %d.%d\n", prop.major, prop.minor);
+				char msg[256];
+				sprintf_s(msg, "--總Global Memory -> %.0f MBytes (%llu bytes)\n",
+					(float)prop.totalGlobalMem / 1048576.0f, (unsigned long long) prop.totalGlobalMem);
+				printf("%s", msg);
+				printf("--%2d 個 Multiprocessors\n", prop.multiProcessorCount);
+				printf("--每個Multiprocessors裡最大執行續數量:%d\n", prop.maxThreadsPerMultiProcessor);
+				printf("--每個Block裡最大執行緒數量:%d\n", prop.maxThreadsPerBlock);
+				printf("--GPU 最大時脈: %.0f MHz (%0.2f GHz)\n", prop.clockRate * 1e-3f, prop.clockRate * 1e-6f);
+
+				break;
+			}
+		}
+	}
+
+	if (i == count)
+	{
+		fprintf(stderr, "No device supporting CUDA 1.x.\n");
+		return false;
+	}
+
+	cudaSetDevice(i);
+
+	return true;
+}
+
+void GenerateNumbers(int *number, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		number[i] = rand() % 10;
+	}
+}
+
+void ArrayCompute()
+{
+	float timeValue;
+
+
+	//-----------------------------------------------
+	cudaEvent_t beginEvent;
+	cudaEvent_t endEvent;
+	cudaEventCreate(&beginEvent);
+	cudaEventCreate(&endEvent);
+	cudaEventRecord(beginEvent, 0);
+	GenerateNumbers(data, DATA_SIZE);
+	int* gpudata, *result;
+	cudaMalloc((void**)&gpudata, sizeof(int)* DATA_SIZE);
+	cudaMalloc((void**)&result, sizeof(int));
+	cudaMemcpy(gpudata, data, sizeof(int)* DATA_SIZE, cudaMemcpyHostToDevice);
+	sumOfSquares << <1, 1, 0 >> >(gpudata, result);
+	int sum;
+	cudaMemcpy(&sum, result, sizeof(int), cudaMemcpyDeviceToHost);
+	cudaFree(gpudata);
+	cudaFree(result);
+	cudaEventRecord(endEvent, 0);
+	cudaEventSynchronize(endEvent);
+	cudaEventElapsedTime(&timeValue, beginEvent, endEvent);
+	cudaEventDestroy(beginEvent);
+	cudaEventDestroy(endEvent);
+	printf("--sum (GPU): %d\n", sum);
+	printf("--執行時間 (GPU): %f\n", float(timeValue) / CLOCKS_PER_SEC);
+	//-----------------------------------------------
+
+	sum = 0;
+	clock_t cpu_time = clock();
+	for (int i = 0; i < DATA_SIZE; i++) {
+		sum += data[i] * data[i];
+	}
+	printf("--sum (CPU): %d\n", sum);
+	printf("--執行時間 (CPU): %f\n", float(clock() - cpu_time) / CLOCKS_PER_SEC);
+}
+
+void ArrayCompute_multiple_threads()
+{
+	float timeValue;
+	//-----------------------------------------------
+	int* gpudata, *result;
+	cudaEvent_t beginEvent;
+	cudaEvent_t endEvent;
+	cudaEventCreate(&beginEvent);
+	cudaEventCreate(&endEvent);
+	cudaEventRecord(beginEvent, 0);
+	GenerateNumbers(data, DATA_SIZE);
+	cudaMalloc((void**)&gpudata, sizeof(int)* DATA_SIZE);
+	cudaMalloc((void**)&result, sizeof(int)* THREAD_NUM);
+	cudaMemcpy(gpudata, data, sizeof(int)* DATA_SIZE, cudaMemcpyHostToDevice);
+
+	sumOfSquares_multiple_threads << <1, THREAD_NUM, 0 >> >(gpudata, result);
+
+	int sum[THREAD_NUM];
+	cudaEventRecord(endEvent, 0);
+	cudaEventSynchronize(endEvent);
+	cudaEventElapsedTime(&timeValue, beginEvent, endEvent);
+	cudaEventDestroy(beginEvent);
+	cudaEventDestroy(endEvent);
+	cudaMemcpy(&sum, result, sizeof(int)* THREAD_NUM, cudaMemcpyDeviceToHost);
+	cudaFree(gpudata);
+	cudaFree(result);
+	cudaFree(time);
+
+	//-----------------------------------------------
+	int final_sum = 0;
+	for (int i = 0; i < THREAD_NUM; i++) {
+		final_sum += sum[i];
+	}
+	printf("sum: %d\n", final_sum);
+	printf("執行時間 (GPU): %f\n", float(timeValue) / CLOCKS_PER_SEC);
+
+	final_sum = 0;
+	clock_t cpu_time = clock();
+	for (int i = 0; i < DATA_SIZE; i++) {
+		final_sum += data[i] * data[i];
+	}
+	printf("sum (CPU): %d\n", final_sum);
+	printf("執行時間 (CPU): %f\n", float(clock() - cpu_time) / CLOCKS_PER_SEC);
+}
+
+/*
+* 原版平方加總程式
+*/
+__global__ static void sumOfSquares(int *num, int* result)
+{
+	int sum = 0;
+	int i;
+	for (i = 0; i < DATA_SIZE; i++) {
+		sum += num[i] * num[i];
+	}
+	*result = sum;
+}
+
+__global__ static void sumOfSquares_multiple_threads(int *num, int* result)
+{
+	const int tid = threadIdx.x;
+	const int size = DATA_SIZE / THREAD_NUM;
+	int sum = 0;
+	int i;
+	for (i = tid * size; i < (tid + 1) * size; i++) {
+		sum += num[i] * num[i];
+	}
+	result[tid] = sum;
+}
